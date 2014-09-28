1010d36fd1829e305762138160a8df732da7692a
David Lu <dvdlu13@gmail.com>
2014-09-28 02:00:51 -0700

    Resolve #21, #22

--- app/scripts/home.js
+++ app/scripts/home.js
@@ -1,54 +1,4 @@
 'use strict';
-$(function() {
-
-    configureKwicks();
-
-});
-
-function configureKwicks() {
-    var $kwicks = $('.kwicks').kwicks({
-        // TODO: show full image
-        maxSize: '50%',
-        behavior: 'menu',
-        duration: 1000
-    });
-
-    // set height, imageWidth, imageMargin during initialization and window resizing
-    setKwicksStyle($kwicks);
-
-    $(window).resize(function() {
-        setKwicksStyle($kwicks);
-    });
-
-    // clear margin when image is expanded
-    $kwicks.on('expand.kwicks', function(e, data) {
-        if (data.expanded) {
-            // reset margin except the expanded image
-            setImageMargin();
-            $(data.expanded).find('img').css({
-                // TODO: needs to be set for smaller screen sizes
-                'margin-left': 0
-            });
-        } else {
-            // reset margin
-            setImageMargin();
-        }
-    });
-
-}
-
-function setKwicksStyle($kwicks) {
-    var kwicksWidth;
-    kwicksWidth = $kwicks.width();
-    $('.kwicks > li > figure > img').css({
-        'height': (200 + kwicksWidth / 5) + 'px'
-    });
-    setImageMargin();
-    $('.about-section').css({
-        'height': $('.kwicks > li').height() + $('.about-section > h2').outerHeight(true) + 40 + 'px'
-    });
-
-}
 
 function setImageMargin() {
     var imageWidth = $('.kwicks > li > figure > img').width();
@@ -74,3 +24,64 @@ function setImageMargin() {
         'margin-left': imageWidth * -0.3 + 'px'
     });
 }
+
+function setKwicksStyle($kwicks) {
+    var kwicksWidth;
+    kwicksWidth = $kwicks.width();
+    $('.kwicks > li > figure > img').css({
+        'height': (200 + kwicksWidth / 5) + 'px'
+    });
+    setImageMargin();
+    $('.about-section').css({
+        'height': $('.kwicks > li').height() + $('.about-section > h2').outerHeight(true) + $('.about-section > p').outerHeight(true) + 80 + 'px'
+    });
+
+}
+
+function configureKwicks() {
+    var $kwicks = $('.kwicks').kwicks({
+        // TODO: show full image
+        maxSize: '50%',
+        behavior: 'menu',
+        duration: 1000
+    });
+
+    // set height, imageWidth, imageMargin during initialization and window resizing
+    setKwicksStyle($kwicks);
+
+    $(window).resize(function() {
+        setKwicksStyle($kwicks);
+    });
+
+    // clear margin when image is expanded
+    $kwicks.on('expand.kwicks', function(e, data) {
+        if (data.expanded) {
+            // reset margin except the expanded image
+            setImageMargin();
+            // shift images to the left for smaller screens
+            if ($(window).width() < 1200) {
+                $(data.expanded).find('#zhiyao, #chen, #yifei').css({
+                    'margin-left': '-50px'
+                });
+                $(data.expanded).find('#xu, #bob').css({
+                    'margin-left': '-150px'
+                });
+                $(data.expanded).find('#david, #ziming').css({
+                    'margin-left': '-200px'
+                });
+            } else {
+                $(data.expanded).find('img').css({
+                    'margin-left': 0
+                });
+            }
+        } else {
+            // reset margin
+            setImageMargin();
+        }
+    });
+
+}
+
+$(function() {
+    configureKwicks();
+});
--- app/scripts/videoModal.js
+++ app/scripts/videoModal.js
@@ -2,7 +2,7 @@
 $(function() {
 
     var videoLink;
-    $(document).on('click', '.btn-default', function () {
+    $(document).on('click', '.video-link', function () {
         videoLink = $(this).data('video-link');
         console.log(videoLink);
 
@@ -10,13 +10,13 @@ $(function() {
 
     // Load iframe using url from data-video-link attribute
     $('#video-modal').on('shown.bs.modal', function() {
-        $('#video-modal .modal-body').append('<iframe id="videoPlayer" src="' + videoLink + '" frameborder="0" width="853px" height="480px"></iframe>');
+        $('#video-modal .modal-body').append('<iframe id="videoPlayer" src="' + videoLink + '?rel=0&showinfo=0&autoplay=1" frameborder="0" allowfullscreen></iframe>');
 
     });
 
     // Remove iframe when modal is closed
     $('#video-modal').on('hidden.bs.modal', function() {
-        $('#video-modal .modal-body').children("iframe").remove();
+        $('#video-modal .modal-body').children('iframe').remove();
     });
 
 });
