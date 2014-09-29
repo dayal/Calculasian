a2bef37d70e56f2fa9f6cbf11dd14e208880a16a
David Lu <dvdlu13@gmail.com>
2014-09-28 17:20:33 -0700

    Resolve #25
    Signed-off-by: David Lu <dvdlu13@gmail.com>

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,14 +2,12 @@
 
 function showEn() {
     document.title = "Calculasian Official Website";
-    $('.btn-default.video-link').text("play");
     $('.cn').hide();
     $('.en').show();
 }
 
 function showCn() {
     document.title = "Calculasian 官方网站";
-    $('.btn-default.video-link').text("播放");
     $('.en').hide();
     $('.cn').show();
 }
