8f2d16b8b23c28a4c5d0c4f08a8ed1d8d56e0916
David Lu <dvdlu13@gmail.com>
2014-09-28 17:11:21 -0700

    Language support and store google fonts locally
    Signed-off-by: David Lu <dvdlu13@gmail.com>

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,27 @@
 'use strict';
+
+function showEn() {
+    document.title = "Calculasian Official Website";
+    $('.btn-default.video-link').text("play");
+    $('.cn').hide();
+    $('.en').show();
+}
+
+function showCn() {
+    document.title = "Calculasian 官方网站";
+    $('.btn-default.video-link').text("播放");
+    $('.en').hide();
+    $('.cn').show();
+}
+
 $(function() {
 
+    showEn();
+    // language toggle
+    $(document).on('click', '.language-cn', function() {
+        showCn();
+    });
+    $(document).on('click', '.language-en', function() {
+        showEn();
+    });
 });
